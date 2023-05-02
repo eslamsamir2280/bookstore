d4a8501532a5fb8f08cb50962cc830b778abd79c
eslam samir <88991029+eslamsamir2280@users.noreply.github.com>
2023-05-02 15:57:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,80 +1,65 @@
-// let books = [];
-// function addbook() {
-//   let id = document.getElementById("id").value;
-//   let title = document.getElementById("title").value;
-//   let author = document.getElementById("author").value;
-//   let qty = document.getElementById("qty").value;
-//   let price = document.getElementById("price").value;
 
-//   // create a new book object
-//   let newBook = { id, title, author, qty, price };
-//   books.push(newBook);
-//   console.log(books);
-//   // add book to the bookstore
-//   // replace this with your own code to add the book to your database or storage
-//   console.log("New Book:", newBook);
-// }
-let bookList = []; // initialize an empty array to store books
+let bookList = []; 
 
 function addbook() {
-  const id = document.getElementById("id").value.trim(); // get the input field value and ignore any leading/trailing white spaces
+  const id = document.getElementById("id").value.trim(); 
   const title = document.getElementById("title").value.trim();
   const author = document.getElementById("author").value.trim();
-  const qty = parseInt(document.getElementById("qty").value.trim(), 10); // convert the quantity value to integer
-  const price = parseFloat(document.getElementById("price").value.trim()); // convert the price value to float
+  const qty = parseInt(document.getElementById("qty").value.trim(), 10); 
+  const price = parseFloat(document.getElementById("price").value.trim()); 
 
-  // check if all required fields are filled
+  
   if (!id || !title || !author || !qty || !price) {
     alert("Please fill all the fields!");
-    return; // exit the function if any of the required fields is missing
+    return; 
   }
 
-  // create a new book object
+  
   const book = { id, title, author, qty, price };
 
-  // add the new book to the bookList array
+  
   bookList.push(book);
   console.log(bookList);
 
-  // reset the input fields
+  
   document.getElementById("id").value = "";
   document.getElementById("title").value = "";
   document.getElementById("author").value = "";
   document.getElementById("qty").value = "";
   document.getElementById("price").value = "";
 
-  // display a success message to the user
+  
   alert("Book added successfully!");
 }
 function deleteBook() {
-  const id = document.getElementById("id").value.trim(); // get the ID of the book to be deleted
+  const id = document.getElementById("id").value.trim(); 
 
-  // find the index of the book with the given ID in the array
+  
   const bookIndex = bookList.findIndex((book) => book.id === id);
 
   if (bookIndex === -1) {
-    // if the book is not found in the array
+    
     alert(`Book with ID ${id} was not found!`);
   } else {
-    // remove the book from the array
+    
     bookList.splice(bookIndex, 1);
 
-    // reset the input field
+    
     document.getElementById("id").value = "";
 
-    // display a success message to the user
+   
     alert(`Book with ID ${id} deleted successfully!`);
   }
   console.log(bookList);
 }
 function findById() {
-  const id = document.getElementById("id").value.trim(); // get the ID of the book to be found
+  const id = document.getElementById("id").value.trim(); 
 
-  // find the book with the given ID in the array
+  
   const book = bookList.find((book) => book.id === id);
 
   if (book) {
-    // if the book is found
+   
     alert(
       `Book details:\nID: ${book.id}\nTitle: ${book.title}\nAuthor: ${book.author}\nQuantity: ${book.qty}\nPrice: ${book.price}`
     );
@@ -83,15 +68,15 @@ function findById() {
   }
 }
 function findByTitle() {
-  const title = document.getElementById("title").value.trim(); // get the title to search for
+  const title = document.getElementById("title").value.trim(); 
 
-  // find all books with the given title in the array
+  
   const books = bookList.filter((book) =>
     book.title.toLowerCase().includes(title.toLowerCase())
   );
 
   if (books.length > 0) {
-    // if any books are found
+    
     let message = `Books with title "${title}":\n`;
     books.forEach((book) => {
       message += `\nID: ${book.id}\nTitle: ${book.title}\nAuthor: ${book.author}\nQuantity: ${book.qty}\nPrice: ${book.price}\n`;
@@ -102,15 +87,15 @@ function findByTitle() {
   }
 }
 function findByAuthor() {
-  const author = document.getElementById("author").value.trim(); // get the author's name to search for
+  const author = document.getElementById("author").value.trim(); 
 
-  // find all books with the given author name in the array
+  
   const books = bookList.filter((book) =>
     book.author.toLowerCase().includes(author.toLowerCase())
   );
 
   if (books.length > 0) {
-    // if any books are found
+    
     let message = `Books by ${author}:\n`;
     books.forEach((book) => {
       message += `\nID: ${book.id}\nTitle: ${book.title}\nAuthor: ${book.author}\nQuantity: ${book.qty}\nPrice: ${book.price}\n`;
@@ -121,18 +106,18 @@ function findByAuthor() {
   }
 }
 function createInvoice() {
-  const title = document.getElementById("title").value.trim(); // get the title of the book
-  const author = document.getElementById("author").value.trim(); // get the author's name
-  const qty = parseInt(document.getElementById("qty").value.trim(), 10); // convert the quantity value to integer
-  const price = parseFloat(document.getElementById("price").value.trim()); // convert the price value to float
+  const title = document.getElementById("title").value.trim(); 
+  const author = document.getElementById("author").value.trim(); 
+  const qty = parseInt(document.getElementById("qty").value.trim(), 10); 
+  const price = parseFloat(document.getElementById("price").value.trim()); 
 
-  // check if all required fields are filled
+  
   if (!title || !author || !qty || !price) {
     alert("Please fill all the fields!");
-    return; // exit the function if any of the required fields is missing
+    return; 
   }
 
-  // find the book with the given title and author in the array
+  
   const book = bookList.find(
     (book) =>
       book.title.toLowerCase() === title.toLowerCase() &&
@@ -140,23 +125,23 @@ function createInvoice() {
   );
 
   if (!book) {
-    // if the book is not found in the array
+    
     alert(`Book "${title}" by ${author} was not found!`);
     return;
   }
 
   if (book.qty < qty) {
-    // if the requested quantity exceeds the available quantity
+    
     alert(
       `Requested quantity is not available! Available quantity is ${book.qty}.`
     );
     return;
   }
 
-  // calculate the total price of the books
+  
   const totalPrice = price * qty;
 
-  // display the invoice to the user
+  
   alert(
     `Invoice for "${title}" by ${author}:\nQuantity: ${qty}\nPrice per unit: ${price}\nTotal Price: ${totalPrice}`
   );
